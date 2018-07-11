2b78d2250c1ba0d11a4878fbb12cf382c2e1c55c
mscherer <mark.scherer@spryker.com>
2018-07-11 16:13:29 +0200

    Fix as per review.

--- src/Controller/HybridAuthController.php
+++ src/Controller/HybridAuthController.php
@@ -50,7 +50,7 @@ class HybridAuthController extends AppController
         if ($user) {
             $this->Auth->setUser($user);
 
-            $this->dispatchEvent('HybridAuth.login', [$user]);
+            $this->dispatchEvent('HybridAuth.login', ['user' => $user]);
 
             return $this->redirect($this->Auth->redirectUrl());
         }
